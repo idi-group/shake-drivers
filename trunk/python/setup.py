a9b9e939ca1af8603e6bbf9ada96798a724ec482
andrew.ramsay@gmail.com <andrew.ramsay@gmail.com@cbcbfa44-3415-11de-8a2e-13aa1706fec1>
2011-08-04 14:39:55 +0000

    added a simple setup.py

---
+++ trunk/python/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+from distutils.command.install import INSTALL_SCHEMES
+import sys, os
+import datetime
+today = datetime.date.today()
+
+for scheme in INSTALL_SCHEMES.values():
+	scheme['data'] = scheme['platlib']
+
+setup(name='pyshake',
+		version='%02d%02d%04d' % (today.day, today.month, today.year),
+		description='Python module for SHAKE SK6/SK7',
+		author='University of Glasgow',
+		author_email='andrew.ramsay@glasgow.ac.uk',
+		url='http://code.google.com/p/shake-drivers',
+		py_modules = ['pyshake', 'pyshake_sk7', 'pyshake_sk7_constants', 'pyshake_sk6', 'pyshake_sk6_constants', 'pyshake_constants', 'pyshake_serial_pc', 'pyshake_sk_common'],
+)
