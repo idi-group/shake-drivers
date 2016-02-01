640b2ab170d0ebee0f99674292ae23591c8726f4
Andrew Ramsay <andrew.ramsay@gmail.com>
2016-02-01 12:47:14 +0000

    bump sdk version in build script

--- cpp/shake_driver/build_osx_64.sh
+++ cpp/shake_driver/build_osx_64.sh
@@ -1,4 +1,4 @@
-CFLAGS="-fPIC -fno-stack-protector -Wno-write-strings -arch x86_64 -mmacosx-version-min=10.5 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk -Iinc"
+CFLAGS="-fPIC -fno-stack-protector -Wno-write-strings -arch x86_64 -mmacosx-version-min=10.5 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk -Iinc"
 LDFLAGS="-lm -lpthread"
 CPP="/usr/bin/g++"
 LIBSHAKE="libshake_driver.so"
@@ -6,4 +6,3 @@ LIBSHAKE="libshake_driver.so"
 rm -f $LIBSHAKE
 
 $CPP -o $LIBSHAKE -shared $CFLAGS src/shake_driver.cpp src/shake_thread.cpp src/shake_serial_usb.cpp src/shake_io.cpp src/shake_packets.cpp src/shake_parsing.cpp src/SK6.cpp src/SK7.cpp src/SHAKE.cpp 
-
